ad9fb4b3f50be621d8f754af9c3c891489914129
Zarnigoor <zarnigorumaraliyeva8@gmail.com>
2025-09-18 22:29:08 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,9 +35,11 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
+  'http://localhost:3003',
   'http://localhost:5173',
   'http://localhost:5174',
   'http://127.0.0.1:3000',
+  'http://127.0.0.1:3003',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174'
 ].filter(Boolean);
